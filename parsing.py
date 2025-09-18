44dc7beaded3c37dd98c1584037a64809f90c0a4
Mark <143651678+markm101@users.noreply.github.com>
2025-09-18 18:05:16 -0400

    Parsing and Updating seperated, updating for all sections added

---
+++ parsing.py
@@ -0,0 +1,42 @@
+import requests
+import bs4
+import datetime
+headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
+worldnews = requests.get('https://feeds.content.dowjones.io/public/rss/RSSWorldNews', headers=headers)
+business = requests.get('https://feeds.content.dowjones.io/public/rss/WSJcomUSBusiness', headers=headers)
+markets = requests.get('https://feeds.content.dowjones.io/public/rss/RSSMarketsMain', headers=headers)
+tech = requests.get('https://feeds.content.dowjones.io/public/rss/RSSWSJD', headers=headers)
+economy = requests.get('https://feeds.content.dowjones.io/public/rss/socialeconomyfeed', headers=headers)
+
+i = 0
+added_headlines = []
+
+month = datetime.datetime.now().strftime("%b")
+day = datetime.datetime.now().strftime("%d")
+def news_list(curr):
+
+    output = []
+    curr.raise_for_status()
+    soup = bs4.BeautifulSoup(curr.content, 'xml')
+
+    for item in soup.find_all('item'):
+        title_name = item.find('title').text.strip()
+        title_date = item.find('pubDate').text
+        title_weekday = title_date[:4]
+        title_month = title_date[8:11]
+        title_year = title_date[11:16]
+        title_day = title_date[5:7]
+        title_hour = title_date[17:19]
+    
+        if (title_month == month) and (title_name not in added_headlines) and title_day == day:
+            output.append([title_name, title_weekday, title_day, title_hour, title_month, title_year])
+            added_headlines.append(title_name)
+
+    articles = sorted(output, key=lambda x: int(x[3]), reverse=True)
+
+    for x in articles:
+        print(x)
+    
+
+
+
---
+++ update.py
@@ -0,0 +1,36 @@
+import requests
+import bs4
+import datetime
+from parsing import news_list
+
+headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
+worldnews = requests.get('https://feeds.content.dowjones.io/public/rss/RSSWorldNews', headers=headers)
+business = requests.get('https://feeds.content.dowjones.io/public/rss/WSJcomUSBusiness', headers=headers)
+markets = requests.get('https://feeds.content.dowjones.io/public/rss/RSSMarketsMain', headers=headers)
+tech = requests.get('https://feeds.content.dowjones.io/public/rss/RSSWSJD', headers=headers)
+economy = requests.get('https://feeds.content.dowjones.io/public/rss/socialeconomyfeed', headers=headers)
+
+
+i = 0
+
+while i < 4:
+    if i == 0:
+        print("worldnews")
+        news_list(worldnews)
+    elif i == 1:
+        print("business")
+        news_list(business)
+    elif i == 2:
+        print("markets")
+        news_list(markets)
+    elif i == 3:
+        print("tech")
+        news_list(tech)
+    elif i == 4:
+        print("economy")
+        news_list(economy)
+
+    i += 1
+
+
+        
