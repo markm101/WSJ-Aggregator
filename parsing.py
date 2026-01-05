0c19ea533ff9ed0b42fa07b073549de359914aa2
Mark <143651678+markm101@users.noreply.github.com>
2026-01-04 22:48:41 -0500

    OOP Rewrite

--- parsing.py
+++ parsing.py
@@ -1,7 +1,6 @@
 import requests
 import bs4
 import datetime
-from database import *
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
 worldnews = requests.get('https://feeds.content.dowjones.io/public/rss/RSSWorldNews', headers=headers)
 business = requests.get('https://feeds.content.dowjones.io/public/rss/WSJcomUSBusiness', headers=headers)
@@ -13,14 +12,71 @@ economy = requests.get('https://feeds.content.dowjones.io/public/rss/socialecono
 
 month = datetime.datetime.now().strftime("%b")
 day = datetime.datetime.now().strftime("%d")
-def news_list(curr, column, choice):
+
+
+
+class article():
+    def __init__(self, title, date):
+        self.title = title
+        self.fulldate = date
+        self.day = date[5:8] 
+        self.month = date[8:11] 
+        self.year = date[11:16]
+        self.weekday = date[:4]
+        self.hour = date[17:19]
+    def getDate(self):
+        #Day, Month, Year
+        return(self.day + self.month + self.year)
+    def getHour(self):
+        return(self.hour)
+    def __str__(self):
+        return (self.title + ' on ' + self.getDate() + ' at ' + self.getHour())
+    
+    def __gt__(self, other):
+        month_dict = {"Jan": 1,
+                      "Feb": 2,
+                      "Mar": 3,
+                      "Apr": 4,
+                      "May": 5,
+                      "Jun": 6,
+                      "Jul": 7,
+                      "Aug": 8,
+                      "Sep": 9,
+                      "Oct": 10,
+                      "Nov": 11,
+                      "Dec": 12}
+        if month_dict[self.month] == month_dict[other.month]:
+            if self.day == other.day:
+                if self.hour >= other.hour:
+                    return True
+            elif self.day > other.day:
+                return True
+        elif month_dict[self.month] < month_dict[other.month]:
+            if self.month == "Jan" and other.month == "Dec":
+                return True
+            else:
+                return False
+        else:
+            return False
+
+
+
+    #title_month = title_date[8:11]
+    #title_year = title_date[11:16]
+    #title_day = title_date[5:7]
+    #title_hour = title_date[17:19]
+
+def news_list(curr, column):
 
     output = []
-    added_headlines = []
     curr.raise_for_status()
     soup = bs4.BeautifulSoup(curr.content, 'xml')
 
     for item in soup.find_all('item'):
+        new_art = article(item.find('title').text.strip(), item.find('pubDate').text)
+        if new_art not in output:
+            output.append(new_art)
+        '''
         title_name = item.find('title').text.strip()
         title_date = item.find('pubDate').text
         title_weekday = title_date[:4]
@@ -28,6 +84,8 @@ def news_list(curr, column, choice):
         title_year = title_date[11:16]
         title_day = title_date[5:7]
         title_hour = title_date[17:19]
+        
+        
     
         if (title_month == month) and (title_name not in added_headlines) and title_day == day:
             output.append([title_name, title_weekday, title_day, title_hour, title_month, title_year, column])
@@ -35,7 +93,9 @@ def news_list(curr, column, choice):
             added_headlines.append(title_name)
 
     articles = sorted(output, key=lambda x: int(x[3]), reverse=True)
-    return (articles)
+    '''
+    output = sorted(output)
+    return ([column, ] + output)
     
 
 
--- update.py
+++ update.py
@@ -2,41 +2,21 @@ import requests
 import bs4
 import datetime
 from parsing import news_list
-from database import *
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
 worldnews = requests.get('https://feeds.content.dowjones.io/public/rss/RSSWorldNews', headers=headers)
 business = requests.get('https://feeds.content.dowjones.io/public/rss/WSJcomUSBusiness', headers=headers)
 markets = requests.get('https://feeds.content.dowjones.io/public/rss/RSSMarketsMain', headers=headers)
 tech = requests.get('https://feeds.content.dowjones.io/public/rss/RSSWSJD', headers=headers)
 economy = requests.get('https://feeds.content.dowjones.io/public/rss/socialeconomyfeed', headers=headers)
+"""
+            news_list(worldnews, "world news")
+            news_list(business, "business")
+            news_list(markets, "markets")
+            news_list(tech, "tech")
+            news_list(economy, "economy")
+"""
 
 
-def update_list(choice): 
-    i = 0
-    total = []
-    make_table(choice)
-    while i < 4:
-        if i == 0:
-            news_list(worldnews, "world news", choice)
-        elif i == 1:
-            news_list(business, "business", choice)
-        elif i == 2:
-            news_list(markets, "markets", choice)
-        elif i == 3:
-            news_list(tech, "tech", choice)
-        elif i == 4:
-            news_list(economy, "economy", choice)
 
-        i += 1
-
-choice = 1
-update_list(1)
-all_articles(1)
-
-choice = 2
-update_list(2)
-print("updated")
-all_articles(2)
-
-
-# Make a compare tables function next, 
+for x in news_list(worldnews, "world news"):
+    print(x)
